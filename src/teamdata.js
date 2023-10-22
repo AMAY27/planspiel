ff91558724961b0e2422bd9acd7080391e88ea01
Amay Rajvaidya <rajvaidyaamay27@gmail.com>
2023-10-22 21:48:02 +0530

    file path issue fixed for vercel

--- src/teamdata.js
+++ src/teamdata.js
@@ -4,35 +4,35 @@ export const teamdata =  [
     {
       name : 'Ajay Dhami',
       role : 'Product Owner',
-      imageUrl : 'src/assets/ajay_profile.jpeg',
+      imageUrl : '/assets/ajay_profile.jpeg',
       linkedurl : 'https://www.linkedin.com/in/ajay-dhami/',
       githuburl : 'https://github.com/AjayDhami'
     },
     {
       name : 'Drashti Patel',
       role : 'Scrum Master',
-      imageUrl : 'src/assets/drashti_profile.jpg',
+      imageUrl : '/assets/drashti_profile.jpg',
       linkedurl : 'https://linkedin.com/in/drashtipatel89',
       githuburl : 'https://github.com/pdrashti8'
     },
     {
       name : 'Kashfa Sehejat Sezuti',
       role : 'Frontend Developer',
-      imageUrl : 'src/assets/kashfa_new.jpeg',
+      imageUrl : '/assets/kashfa_new.jpeg',
       linkedurl : 'https://www.linkedin.com/in/kashfa-sehejat-sezuti/',
       githuburl : 'https://github.com/kashfasehejatsezuti'
     },
     {
       name : 'Prabudh Mishra',
       role : 'FullStack Developer',
-      imageUrl : 'src/assets/prabudh_profile.jpeg',
+      imageUrl : '/assets/prabudh_profile.jpeg',
       linkedurl : 'https://www.linkedin.com/in/prabudhmishra',
       githuburl : 'https://www.github.com/prabudh-mishra'
     },
     {
       name : 'Amay Rajvaidya',
       role : 'Fullstack Developer',
-      imageUrl : 'src/assets/amay_profile.jpeg',
+      imageUrl : '/assets/amay_profile.jpeg',
       linkedurl : 'https://www.linkedin.com/in/amay-rajvaidya-886291188',
       githuburl : 'https://www.github.com/AMAY27'
     }
