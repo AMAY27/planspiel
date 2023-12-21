a5cebf16811df87cfcd1c0cfc5c848061350bc9c
AMAY27 <51704191+AMAY27@users.noreply.github.com>
2023-12-22 02:12:46 +0530

    Innovation content update (#9)
    * Innovation content updates and changes
    
    * Images path updates in Informative sections
    
    ---------
    
    Co-authored-by: Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>

--- src/Components/Updates.jsx
+++ src/Components/Updates.jsx
@@ -10,7 +10,7 @@ const Updates = () => {
             <h1>V-Tenet</h1>
         </div>
         <div className='content-div'>
-          <div className="img-div"><img src="../../public/assets/Innovation_img.png"/></div>
+          <div className="img-div"><img src="/assets/Innovation_img.png"/></div>
           <div className='content'>
             <p>
             Our innovative approach goes beyond traditional methods, utilizing the power of automation using Machine Learning and Web scraping to stay ahead of evolving digital challenges. Join us in embracing a future where technology meets integrity, and where our certification becomes synonymous with a safer and more ethical online space. Experience innovation at its finest.
